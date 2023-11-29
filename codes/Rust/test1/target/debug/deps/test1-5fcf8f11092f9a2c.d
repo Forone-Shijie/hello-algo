da70e7ec5a052edacf11a23c13a74dd554b54f22
Forone-Shijie <2263224042@qq.com>
2023-11-30 00:16:17 +0800

    test rust on macbook

---
+++ codes/Rust/test1/target/debug/deps/test1-5fcf8f11092f9a2c.d
@@ -0,0 +1,5 @@
+/Users/forone/Project/Hello-algo/hello-algo/codes/Rust/test1/target/debug/deps/test1-5fcf8f11092f9a2c: src/main.rs
+
+/Users/forone/Project/Hello-algo/hello-algo/codes/Rust/test1/target/debug/deps/test1-5fcf8f11092f9a2c.d: src/main.rs
+
+src/main.rs:
---
+++ codes/Rust/test1/target/debug/test1.d
@@ -0,0 +1,1 @@
+/Users/forone/Project/Hello-algo/hello-algo/codes/Rust/test1/target/debug/test1: /Users/forone/Project/Hello-algo/hello-algo/codes/Rust/test1/src/main.rs
