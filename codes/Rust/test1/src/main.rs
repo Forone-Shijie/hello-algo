cf4f6cacffe1b3d36e295f8a8f6b6b5e26ce2591
Forone-Shijie <2263224042@qq.com>
2023-12-02 13:49:33 +0800

    'code test on rust'

--- codes/Rust/test1/src/main.rs
+++ codes/Rust/test1/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+// for loop
+// fn for_loop(n: i32) -> i32{
+//     let mut res = 0;
+
+//     for i in 1..=n{
+//         res += i;
+//     }
+
+//     res
+// }
+
+// fn main(){
+
+//     let result = for_loop(3);
+//     println!("result = {0}, {1}", result, for_loop(4));
+// }
+
+
+fn while_loop(n: i32) -> i32{
+    let mut res = 0;
+    let mut i = 0;
+
+    while i<=n {
+        res += i;
+        i+=1;
+    }
+
+    res
+}
+
+fn main(){
+    println!("Result = {}", while_loop(5));
 }
