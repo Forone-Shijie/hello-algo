c658ce7fc90669445834e07cc8d24ae8931bfd50
Forone-Shijie <2263224042@qq.com>
2023-12-06 23:16:50 +0800

    Update the Chapter 2.2

--- codes/CPP/test.cpp
+++ codes/CPP/test.cpp
@@ -3,18 +3,28 @@
 #include <sstream>
 #include <string>
 
-std::string nestedForloop(int n){
-    std::ostringstream res;
+// std::string nestedForloop(int n){
+//     std::ostringstream res;
 
-    for (int i=1; i<=n; ++i){
-        for (int j=1; j<=n; ++j){
-            res << "(" << i << ", " << j << "), ";
-        }
+//     for (int i=1; i<=n; ++i){
+//         for (int j=1; j<=n; ++j){
+//             res << "(" << i << ", " << j << "), ";
+//         }
+//     }
+//     return res.str();
+// }
+
+int fib(int n){
+    if (n == 1 || n==2){
+        return n-1;
     }
-    return res.str();
+
+    int res = fib(n-1) + fib(n-2);
+
+    return res;
 }
 
 int main(){
     int n = 5;
-    std::cout << nestedForloop(n) << std::endl;
+    std::cout << "Result = " << fib(n) << std::endl;
 }
--- codes/Rust/test1/src/main.rs
+++ codes/Rust/test1/src/main.rs
@@ -16,18 +16,38 @@
 // }
 
 
-fn while_loop(n: i32) -> i32{
-    let mut res = 0;
-    let mut i = 0;
+// fn while_loop(n: i32) -> i32{
+//     let mut res = 0;
+//     let mut i = 0;
 
-    while i<=n {
-        res += i;
-        i+=1;
+//     while i<=n {
+//         res += i;
+//         i+=1;
+//     }
+
+//     res
+// }
+
+// fn recur(n: i32) -> i32{
+//     if n == 1{ //the condition of ending
+//         return 1; 
+//     }
+
+//     let result = n* recur(n-1); // recur method
+
+//     result
+// }
+
+fn fib(n: i32) -> i32{
+    if n==1 || n==2{
+        return n-1;
     }
+
+    let res = fib(n-1) + fib(n-2);
 
     res
 }
 
 fn main(){
-    println!("Result = {}", while_loop(5));
+    println!("Result = {}", fib(5));
 }
